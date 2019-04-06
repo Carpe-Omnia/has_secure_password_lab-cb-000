0ea1a64936bf7b4d81a081c31147a00666f9a5db
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-06 22:34:18 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_06_222148) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "password_digest"
+    t.string "username"
+  end
 
 end
